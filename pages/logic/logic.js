93a9a64a7d835db1ffe452ba2b4f4f2f4fb6fad6
xof96 <blackmathix@gmail.com>
2018-05-17 17:02:00 -0400

    path fixed

--- pages/logic/logic.js
+++ pages/logic/logic.js
@@ -1,7 +1,7 @@
 $(function() {
   $("#mb-Matias").click(function() {
     if ($(this).siblings().length == 0){
-      var txt = $("<div class='col-8 col-s-12'></div>").load("http://dinosdcc.me/pages/Matias/index.html #mb-info");
+      var txt = $("<div class='col-8 col-s-12'></div>").load("Matias/index.html #mb-info");
       $(this).after(txt);
     }
     else {
